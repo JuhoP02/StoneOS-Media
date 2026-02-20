a9c15754769eb2fd3a7bab8c089e77fd2947e863
JuhoP02 <125037081+JuhoP02@users.noreply.github.com>
2026-02-20 18:40:30 +0200

    Fixed paths of media files

--- media_index.lua
+++ media_index.lua
@@ -1,46 +1,46 @@
 {
   packs = {
     {
-      id = "SOUP",
-      title = "SOUP",
-      base = "https://raw.githubusercontent.com/JuhoP02/StoneOS-Media/main/SOUP/",
-      files = { {name="SOUP.svid"}, {name="SOUP.dfpwm"}, {name="SOUP.sva"} }
+      id="SOUP",
+      title="SOUP",
+      base="https://github.com/JuhoP02/StoneOS-Media/raw/main/media/SOUP/",
+      files={ {name="SOUP.svid"}, {name="SOUP.dfpwm"}, {name="SOUP.sva"} }
     },
     {
-      id = "Rick",
-      title = "Rick Roll",
-      base = "https://raw.githubusercontent.com/JuhoP02/StoneOS-Media/main/Rick/",
-      files = { {name="rick_roll.svid"}, {name="rick_roll.dfpwm"}, {name="rick_roll.sva"} }
+      id="Rick",
+      title="Rick Roll",
+      base="https://github.com/JuhoP02/StoneOS-Media/raw/main/media/Rick/",
+      files={ {name="rick_roll.svid"}, {name="rick_roll.dfpwm"}, {name="rick_roll.sva"} }
     },
     {
-      id = "Buttmen",
-      title = "Buttmen",
-      base = "https://raw.githubusercontent.com/JuhoP02/StoneOS-Media/main/Buttmen/",
-      files = { {name="buttmen.svid"}, {name="buttmen.dfpwm"}, {name="buttmen.sva"} }
+      id="Buttmen",
+      title="Buttmen",
+      base="https://github.com/JuhoP02/StoneOS-Media/raw/main/media/Buttmen/",
+      files={ {name="buttmen.svid"}, {name="buttmen.dfpwm"}, {name="buttmen.sva"} }
     },
     {
-      id = "Cinema",
-      title = "Cinema",
-      base = "https://raw.githubusercontent.com/JuhoP02/StoneOS-Media/main/Cinema/",
-      files = { {name="cinema.svid"}, {name="cinema.dfpwm"}, {name="cinema.sva"} }
+      id="Cinema",
+      title="Cinema",
+      base="https://github.com/JuhoP02/StoneOS-Media/raw/main/media/Cinema/",
+      files={ {name="cinema.svid"}, {name="cinema.dfpwm"}, {name="cinema.sva"} }
     },
     {
-      id = "Paleorock",
-      title = "Paleorock",
-      base = "https://raw.githubusercontent.com/JuhoP02/StoneOS-Media/main/Paleorock/",
-      files = { {name="paleorock.svid"}, {name="paleorock.dfpwm"}, {name="paleorock.sva"} }
+      id="Paleorock",
+      title="Paleorock",
+      base="https://github.com/JuhoP02/StoneOS-Media/raw/main/media/Paleorock/",
+      files={ {name="paleorock.svid"}, {name="paleorock.dfpwm"}, {name="paleorock.sva"} }
     },
     {
-      id = "WANO",
-      title = "WANO",
-      base = "https://raw.githubusercontent.com/JuhoP02/StoneOS-Media/main/WANO/",
-      files = { {name="WANO.svid"}, {name="WANO.dfpwm"}, {name="WANO.sva"} }
+      id="WANO",
+      title="WANO",
+      base="https://github.com/JuhoP02/StoneOS-Media/raw/main/media/WANO/",
+      files={ {name="WANO.svid"}, {name="WANO.dfpwm"}, {name="WANO.sva"} }
     },
     {
-      id = "Shrek",
-      title = "Shrek",
-      base = "https://raw.githubusercontent.com/JuhoP02/StoneOS-Media/main/Shrek/",
-      files = { {name="shrek.svid"}, {name="shrek.dfpwm"}, {name="shrek.sva"} }
+      id="Shrek",
+      title="Shrek",
+      base="https://github.com/JuhoP02/StoneOS-Media/raw/main/media/Shrek/",
+      files={ {name="shrek.svid"}, {name="shrek.dfpwm"}, {name="shrek.sva"} }
     },
   }
 }
